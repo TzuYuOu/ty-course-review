fea8757696ca8c7a29062033f10d0b847c1972dc
TzuYuOu <a1225johnny@gmail.com>
2021-07-23 11:55:27 +0800

    Update course route

--- frontend/src/services/course-service.js
+++ frontend/src/services/course-service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 class CourseService {
   http = axios.create({
-    baseURL: `http://localhost:5000/api/courses`,
+    baseURL: `/api/courses`,
     headers: {
       "Content-type": "application/json"
     }
